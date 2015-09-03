57c4a43d33846658ecb38aa09f7e2ae36507115d
Boris Spektor <boris@soom.la>
2015-09-03 21:02:05 +0300

    - Added fp=true flag to OnMarketPurchaseStarted event - Changed version to 3.6.13 - Rebuilt libs

--- SoomlaiOSStore/SoomlaStore.m
+++ SoomlaiOSStore/SoomlaStore.m
@@ -34,7 +34,7 @@
 
 #import "SoomlaVerification.h"
 
-#define SOOMLA_STORE_VERSION @"3.6.12"
+#define SOOMLA_STORE_VERSION @"3.6.13"
 
 @interface SoomlaStore (){
     NSMutableArray* verifications;
